2c322849cb9420ab16483658e48418cc16ecb902
PaperCIoud <haoni.amy@gmail.com>
2024-02-02 22:21:38 -0500

    initial UI setup with textinputs

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Keyboard, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { DataStore } from '@aws-amplify/datastore';
 import {Note} from './src/models';
 
 
 export default function App() {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputNote, setInputNote] = useState("");
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => Keyboard.dismiss()}
+        
+      >
+        <Text>Enter Notes</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="title"
+          onChangeText={newText => setInputTitle(newText)}
+          defaultValue={inputTitle}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="type notes"
+          onChangeText={newText => setInputNote(newText)}
+          defaultValue={inputNote}
+          multiline={true}
+        />
+      </TouchableOpacity>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -25,5 +50,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  input: {
+    borderWidth: 1,
+    width: 350,
+    marginBottom: 10
+  }
 });
